127d3ade07289b15c12b64c5cd664d4beb9d10ef
Eric Vaandering <ericvaandering@gmail.com>
2022-05-09 16:18:41 -0500

    Remove DocDB public

--- docker/cms-entrypoint.sh
+++ docker/cms-entrypoint.sh
@@ -4,7 +4,6 @@
 
 cp /var/www/html/DocDB/Static/css/CMS* /data/DocDB/Static/css/
 j2 /tmp/ProdGlobals.pm.j2  > /var/www/cgi-bin/DocDB/ProdGlobals.pm
-j2 /tmp/PublicGlobals.pm.j2  > /var/www/cgi-bin/PublicDocDB/PublicGlobals.pm
 
 chown -R apache:apache /data/DocDB/
 
