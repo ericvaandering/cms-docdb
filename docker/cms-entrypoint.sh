aee5f24c111508cdefb3bf9f84263c0fd74f7a52
Eric Vaandering <ericvaandering@gmail.com>
2026-01-14 15:20:48 -0600

    Switch CMS from shibboleth to oidc
    Also add a Hello re-direct script which is needed but unused, really.

--- docker/cms-entrypoint.sh
+++ docker/cms-entrypoint.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-/usr/sbin/shibd &
-
 cp /var/www/html/DocDB/Static/css/CMS* /data/DocDB/Static/css/
 j2 /tmp/ProdGlobals.pm.j2  > /var/www/cgi-bin/DocDB/ProdGlobals.pm
 j2 /tmp/PublicGlobals.pm.j2  > /var/www/cgi-bin/PublicDocDB/PublicGlobals.pm
-
+cp /Hello  /var/www/cgi-bin/DocDB/
+chmod +x /var/www/cgi-bin/DocDB/Hello
 chown -R apache:apache /data/DocDB/
+
+export PERL5LIB=/var/www/cgi-bin/DocDB
 
 /docker-entrypoint.sh
