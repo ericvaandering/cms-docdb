f0ced8b657d55181959433b9ffce1859d2a49657
Eric Vaandering <ewv@fnal.gov>
2026-01-29 14:30:21 -0600

    Redirect to right place

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-podman build . -t registry.cern.ch/cms-docdb/cms_docdb:2026.0.1
-podman push registry.cern.ch/cms-docdb/cms_docdb:2026.0.1
+podman build . -t registry.cern.ch/cms-docdb/cms_docdb:2026.1.0
+podman push registry.cern.ch/cms-docdb/cms_docdb:2026.1.0
